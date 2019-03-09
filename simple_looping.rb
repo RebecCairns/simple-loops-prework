2f67fcbd08804b91d533e3feebc2e825662f6361
Rebecca Cairns <rebecca_cairns@yahoo.com>
2019-03-09 22:11:11 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -28,8 +28,9 @@ end
 def until_iterator(number)
   count = 1
   while count <= number 
-  puts "Welcome to Flatiron School's Web Development Course!"
-
+     puts "Welcome to Flatiron School's Web Development Course!"
+     count += 1 
+  end
 end
 
 def for_iterator(number_of_times)
